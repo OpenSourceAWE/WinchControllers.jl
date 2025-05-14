699c9f3792a912027b4beaaa08f151fec40df355
Uwe Fechner <u.fechner-1@tudelft.nl>
2025-05-14 15:33:20 +0200

    Some progress

--- src/WinchControllers.jl
+++ src/WinchControllers.jl
@@ -1,11 +1,16 @@
 module WinchControllers
 
-using Parameters, StructTypes
+using Parameters, StructTypes, KiteUtils, WinchModels, StaticArrays
 
 export WCSettings
 
+abstract type AbstractForceController end
+const AFC = AbstractForceController
+
 # Write your package code here.
 include("utils.jl")
+include("components.jl")
 include("wc_settings.jl")
+include("wc_components.jl")
 
 end
