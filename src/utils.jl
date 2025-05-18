8406ea3305378d887fd31e0d11211f1db50d89b6
Uwe Fechner <fechner@aenarete.eu>
2025-05-18 18:10:15 +0200

    Clearer definition of @limit

--- src/utils.jl
+++ src/utils.jl
@@ -23,19 +23,26 @@ function saturate(value, min_, max_)
 end
 
 """
-    macro limit(name, min, max=nothing)
+    macro limit(name, minmax, max=nothing)
 
 Limit the value of a variable. 
+
+Parameters:
+- name: the name of the scalar variable that shall be limited
+- minmax: if max is provided, this is the lower value to which the variable is clamped, otherwise it is the upper value
+- max: the upper value to which to limit the provided variable or nothing
 
 Usage:
 - @limit x 1 4 # limits the value to the range    1 .. 4,  modifies x
 - @limit x 10  # limits the value to the range -inf .. 10, modifies x
 
 """
-macro limit(name, min, max=nothing)
+macro limit(name, minmax, max=nothing)
     if isnothing(max)
-        max = min
-        min = :(typemin($min))
+        max = minmax
+        min = :(typemin($minmax))
+    else
+        min = minmax
     end
 
     return esc( :($name = clamp($name, $min, $max)) )
