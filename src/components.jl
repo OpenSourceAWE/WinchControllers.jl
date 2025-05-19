ef638958aae25509c7f6d87448ef89cedcb79344
Uwe Fechner <fechner@aenarete.eu>
2025-05-19 09:51:43 +0200

    Add example for Mixer_3CH

--- src/components.jl
+++ src/components.jl
@@ -398,6 +398,31 @@ Mix three analog inputs. Implements the following block diagram: ![mixer_3ch](as
 # Fields
 
 $(TYPEDFIELDS)
+
+# Example
+```julia
+using WinchControllers, ControlPlots
+
+TIME = range(0.0, 10, 501)
+SIN1 = sin.(TIME * 0.5 * 2pi) * 2
+SIN2 = sin.(TIME * 0.25 * 2pi)
+NOISE = (rand(501).-0.5) * 2
+mix3 = Mixer_3CH(10, 0.25)
+out  = zeros(501)
+
+for i in 1:501                   # 10s with dt=0.02
+    if i == 150
+        select_b(mix3, true)
+    elseif i == 300
+        select_c(mix3, true)
+    elseif i == 450
+        select_c(mix3, false)        
+    end
+    out[i] = calc_output(mix3, SIN1[i], SIN2[i], NOISE[i])
+    on_timer(mix3)
+end
+plot(TIME, out)
+```
 """
 @with_kw mutable struct Mixer_3CH @deftype Float64
     dt
