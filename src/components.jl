8249173d28464d669b404127bf1f413cb5934f9b
Uwe Fechner <fechner@aenarete.eu>
2025-05-19 09:35:29 +0200

    Add example for Mixer_2D

--- src/components.jl
+++ src/components.jl
@@ -287,11 +287,30 @@ end
 """
     mutable struct Mixer_2CH
 
-Mix two analog inputs. Implements the following block diagram: ![mixer_2ch](assets/mixer_2ch.png).
+Mix two analog inputs.  It selects either input a or input b depending on the value of the digital input and implements 
+soft switching with a blend time `t_blend`. Implements the following block diagram: ![mixer_2ch](assets/mixer_2ch.png).
 
 # Fields
 
 $(TYPEDFIELDS)
+
+# Example
+```julia
+using WinchControllers, Test
+
+m2 = Mixer_2CH(0.2, 1.0) # dt=0.2s, t_blend = 1.0s
+x = ones(10)
+y = 2*x
+out = zeros(10)
+for i in 1:length(x)
+    in1=x[i]
+    in2=y[i]
+    out[i] = calc_output(m2, x[i], y[i])
+    select_b(m2, i > 2)
+    on_timer(m2)
+end
+@test all(out .≈ [1.0, 1.0, 1.0, 1.2, 1.4, 1.6, 1.8, 2.0, 2.0, 2.0])
+```
 """
 @with_kw mutable struct Mixer_2CH @deftype Float64
     dt
