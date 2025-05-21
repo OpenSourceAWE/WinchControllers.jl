61901ff561eabfa68170ae1eda4f27ccd017f65a
Uwe Fechner <u.fechner-1@tudelft.nl>
2025-05-21 06:10:51 +0200

    Cleanup

--- src/WinchControllers.jl
+++ src/WinchControllers.jl
@@ -12,7 +12,7 @@ export get_speed, set_force, calc_v_set, set_v_set, on_timer, get_acc, get_state
 export select_b, select_c, reset, calc_output
 export merge_angles, @limit
 export SpeedController, set_inactive, set_v_act, set_v_set, set_v_set_in, set_tracking, get_v_set_out
-export LowerForceController, SpeedController, UpperForceController
+export LowerForceController, UpperForceController
 
 abstract type AbstractForceController end
 const AFC = AbstractForceController
