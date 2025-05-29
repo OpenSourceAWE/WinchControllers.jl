8cc4a14947591d4a67e9fbaf386f61d5f39c8ba0
Uwe Fechner <u.fechner-1@tudelft.nl>
2025-05-29 20:58:08 +0200

    Some progress

--- examples/test_winchcontroller.jl
+++ examples/test_winchcontroller.jl
@@ -58,22 +58,14 @@ for i in 1:length(lg)
     on_timer(winch)
     on_timer(wc)
 
-    # calculate some values for logging
-    state  = get_state(wc)
+    # calculate values for logging
     status = get_status(wc)
-    force  = status[3]
+    force  = wc.ufc.force
     f_set  = status[4]
-    if state in [0,2]
-        v_set_in = NaN
-    else
-        v_set_in = wc.sc.v_set_in
-    end
-    f_err = get_f_err(wc)
-    v_set = get_v_set(wc)
     
     # log the values
-    log(lg; v_ro=v_act, acc=get_acc(winch), state, reset=status[1], active=status[2], 
-            force, f_set, f_err, v_err=get_v_err(wc), v_set, v_set_out, v_set_in)
+    log(lg; v_ro=v_act, acc=get_acc(winch), state=get_state(wc), reset=status[1], active=status[2], force, 
+        f_set, f_err=get_f_err(wc), v_err=get_v_err(wc), v_set=get_v_set(wc), v_set_out, v_set_in=get_v_set_in(wc))
 end
 
 # plot the results  
--- src/WinchControllers.jl
+++ src/WinchControllers.jl
@@ -10,9 +10,9 @@ import Base.length
 export WCSettings, WinchController, Winch, update
 export Mixer_2CH, Mixer_3CH, Integrator, UnitDelay, RateLimiter, CalcVSetIn
 export set_f_set, set_v_sw, set_reset, set_inactive, set_vset_pc
-export set_tracking, set_f_set, get_v_set, get_v_err, calc_vro, get_f_err
+export set_tracking, set_f_set, get_v_set, get_v_set_in, get_v_err, calc_vro, get_f_err
 export get_speed, set_force, calc_v_set, set_v_set, on_timer, get_acc, get_state, get_status
-export select_b, select_c, reset, calc_output
+export select_b, select_c, reset, calc_output 
 export merge_angles, @limit, saturate
 export SpeedController, set_inactive, set_v_act, set_v_set, set_v_set_in, set_tracking, get_v_set_out
 export LowerForceController, UpperForceController
