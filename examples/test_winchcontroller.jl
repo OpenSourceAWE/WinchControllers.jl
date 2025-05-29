b11574895721a8cccc6158f87493cc1a5f602245
Uwe Fechner <u.fechner-1@tudelft.nl>
2025-05-29 21:20:42 +0200

    Cleanup

--- examples/test_winchcontroller.jl
+++ examples/test_winchcontroller.jl
@@ -58,14 +58,12 @@ for i in 1:length(lg)
     on_timer(winch)
     on_timer(wc)
 
-    # calculate values for logging
+    # get values for logging
     status = get_status(wc)
-    force  = wc.ufc.force
-    f_set  = status[4]
     
     # log the values
-    log(lg; v_ro=v_act, acc=get_acc(winch), state=get_state(wc), reset=status[1], active=status[2], force, 
-        f_set, f_err=get_f_err(wc), v_err=get_v_err(wc), v_set=get_v_set(wc), v_set_out, v_set_in=get_v_set_in(wc))
+    log(lg; v_ro=v_act, acc=get_acc(winch), state=get_state(wc), reset=status[1], active=status[2], force=status[3], 
+        f_set=status[4], f_err=get_f_err(wc), v_err=get_v_err(wc), v_set=get_v_set(wc), v_set_out, v_set_in=get_v_set_in(wc))
 end
 
 # plot the results  
