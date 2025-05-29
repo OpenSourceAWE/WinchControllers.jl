356a6d4b3281ba1852defdc8e8b09e471c03ff43
Uwe Fechner <u.fechner-1@tudelft.nl>
2025-05-29 05:57:02 +0200

    Export saturate

--- src/WinchControllers.jl
+++ src/WinchControllers.jl
@@ -13,7 +13,7 @@ export set_f_set, set_v_sw, set_reset, set_inactive, set_vset_pc
 export set_tracking, set_f_set, get_v_err, calc_vro, get_f_err
 export get_speed, set_force, calc_v_set, set_v_set, on_timer, get_acc, get_state, get_status
 export select_b, select_c, reset, calc_output
-export merge_angles, @limit
+export merge_angles, @limit, saturate
 export SpeedController, set_inactive, set_v_act, set_v_set, set_v_set_in, set_tracking, get_v_set_out
 export LowerForceController, UpperForceController
 export get_startup, get_triangle_wind
