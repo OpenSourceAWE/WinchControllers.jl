37bc500f2073fcb0c2ba31082b8d27865b5007a3
Uwe Fechner <u.fechner-1@tudelft.nl>
2025-05-31 10:32:15 +0200

    Cleanup

--- scripts/autotune.jl
+++ scripts/autotune.jl
@@ -1,10 +1,10 @@
-# this script tunes the controller parameters (well, seven of them)
+# this script tunes the controller parameters (well, eight of them)
 using Pkg
 if ! ("ControlPlots" ∈ keys(Pkg.project().dependencies))
     using TestEnv; TestEnv.activate()
     using Test
 end
-using WinchControllers, KiteUtils, PRIMA, ControlPlots
+using WinchControllers, KiteUtils, PRIMA, NOMAD, ControlPlots
 
 TUNED::Bool = false
 load_settings("system.yaml")
@@ -66,47 +66,41 @@ function simulate(wcs::WCSettings; return_lg::Bool = false)
 
         # get values for logging and log them
         status = get_status(wc)
-        log(lg; v_wind, v_ro=v_act, acc=get_acc(winch), state=get_state(wc), reset=status[1], active=status[2], force=status[3], 
-            f_set=status[4], f_err=get_f_err(wc), v_err=get_v_err(wc), v_set=get_v_set(wc), v_set_out, v_set_in=get_v_set_in(wc))
+        log(lg; v_wind, v_ro=v_act, acc=get_acc(winch), state=get_state(wc), reset=status[1], active=status[2], 
+            force=status[3], f_set=status[4], f_err=get_f_err(wc), v_err=get_v_err(wc), v_set=get_v_set(wc), 
+            v_set_out, v_set_in=get_v_set_in(wc), jerk=winch.jerk, p_dyn=winch.p_dyn)
     end
     if return_lg
         return wcs, lg
     end
     # calculate the performance metrics
-    -gamma(lg)
+    -gamma(lg; jerk=true)
 end
 
-
-function simulate_sc(x::Vector{Cdouble}; return_lg::Bool = false)
+function simulate_all(x::Vector; return_lg::Bool = false)
     wcs = WCSettings(dt=0.02)
     update(wcs)
     wcs.test = true
     wcs.i_speed = x[1] # set the speed controller gain
     wcs.p_speed = x[2] # set the speed controller proportional gain
     wcs.t_blend = x[3] # set the blending time for switching between controllers
+    wcs.pf_low  = x[4] # set the speed controller gain
+    wcs.if_low  = x[5] # set the speed controller proportional gain
+    wcs.pf_high = x[6] # set the lower force controller gain
+    wcs.if_high = x[7] # set the lower force controller integral gain
+    wcs.df_high = x[8]
     simulate(wcs; return_lg)
 end
 
-function simulate_lfc(x::Vector{Cdouble}; return_lg::Bool = false)
-    wcs = WCSettings(dt=0.02)
-    update(wcs)
-    wcs.test = true
-    wcs.pf_low = x[1] # set the lower force controller gain
-    wcs.if_low = x[2] # set the lower force controller integral gain
-    simulate(wcs; return_lg)
+function eval_fct(x)
+  bb_outputs = [simulate_all(x)]
+  success = bb_outputs[1] < -0.7
+  count_eval = true
+  return (success, count_eval, bb_outputs)
 end
 
-function simulate_ufc(x::Vector{Cdouble}; return_lg::Bool = false)
-    wcs = WCSettings(dt=0.02)
-    update(wcs)
-    wcs.test = true
-    wcs.pf_high = x[1] # set the higher force controller gain
-    wcs.if_high = x[2] # set the higher force controller integral gain
-    simulate(wcs; return_lg)
-end
-
-function autotune(controller::WinchControllerState)
-    global x, info, lg, TUNED
+function autotune(max_iter=1000)
+    global x, lg, TUNED, result
     if TUNED
         load_settings("system_tuned.yaml")
     else
@@ -115,74 +109,40 @@ function autotune(controller::WinchControllerState)
     wcs = WCSettings(dt=0.02)
     update(wcs)
     wcs.test = true
-    if controller == wcsSpeedControl
-        println("Autotuning speed controller...")
-        # Define the parameters for the autotuning
-        x0 = [wcs.p_speed, wcs.i_speed, wcs.t_blend] # initial guess for the speed controller gain
-        x, info = bobyqa(simulate_sc, x0;
-            xl = 0.75 .* x0,
-            xu = 1.5 .* x0,
-            rhobeg = maximum([1e-4, minimum(x0)/4]),
-            npt=10,
-            maxfun = 500
-        )
-    elseif controller == wcsLowerForceLimit
-        println("Autotuning lower force limit controller...")
-        # Define the parameters for the autotuning
-        x0 = [maximum([1e-4, wcs.pf_low]), wcs.if_low] # initial guess for the speed controller gain
+    println("Autotuning all controllers...")
+    x0 = [wcs.p_speed, wcs.i_speed, wcs.t_blend, wcs.pf_low, wcs.if_low, wcs.pf_high, wcs.if_high, maximum([1e-5, wcs.df_high])] # initial guess for the speed controller gain
 
-        x, info = bobyqa(simulate_lfc, x0;
-            xl = 0.25 .* x0,
-            xu = 2.0 .* x0,
-            rhobeg = maximum([1e-4, minimum(x0)/4]),
-            maxfun = 500
-        )
-    elseif controller == wcsUpperForceLimit
-        println("Autotuning upper force control...")
-        # Define the parameters for the autotuning
-        x0 = [maximum([2e-5, wcs.pf_high]), wcs.if_high] # initial guess for the speed controller gain
-        x, info = bobyqa(simulate_lfc, x0;
-            xl = 0.25 .* x0,
-            xu = 2.0 .* x0,
-            rhobeg = maximum([2e-5, minimum(x0)/4]),
-            maxfun = 500
-        )
-    else
-        error("Unknown controller state: $controller")
-        return
-    end
-
+    pb = NomadProblem(8,         # number of inputs of the blackbox
+                      1,         # number of outputs of the blackbox
+                      ["OBJ"],   # type of outputs of the blackbox
+                      eval_fct;
+                      lower_bound = 0.5 .* x0,
+                      upper_bound = 2.0 .* x0)
+    pb.options.max_bb_eval = max_iter 
+    result = solve(pb, x0)
+    x = result.x_best_feas
+    
     println("Autotuning results: $x")
-    println("Iterations: $(info.nf)")
-    println(PRIMA.reason(info.status))
 
-    if issuccess(info)
-        println("Running simulation with tuned parameters...")
-        TUNED = true
-        if controller == wcsSpeedControl
-            wcs, lg = simulate_sc(x; return_lg=true)
-        elseif controller == wcsLowerForceLimit
-            wcs, lg = simulate_lfc(x; return_lg=true)
-        elseif controller == wcsUpperForceLimit
-            wcs, lg = simulate_ufc(x; return_lg=true)
-        end
+    println("Running simulation with tuned parameters...")
+    TUNED = true
+    wcs, lg = simulate_all(x; return_lg=true)
 
-        println("\nPerformance of force controllers: $(round(100*(1-f_err(lg)), digits=2)) %")
-        println("Performance of speed controller:  $(round(100*(1-v_err(lg)), digits=2)) %")
-        println("Damage:                           $(round(100*(damage(lg)), digits=2)) %")
-        println("Combined performance γ: $(round(-100*info.fx, digits=2)) %")  
-        return wcs
-    else
-        println("Autotuning failed: $(PRIMA.reason(info.status))")
-        return nothing
-    end 
+    println("\nPerformance of force controllers: $(round(100*(1-f_err(lg)), digits=2)) %")
+    println("Performance of speed controller:  $(round(100*(1-v_err(lg)), digits=2)) %")
+    println("Damage:                           $(round(100*(damage(lg)), digits=2)) %")
+    println("Damage with rms:                  $(round(100*(damage(lg; rms=true)), digits=2)) %")
+    println("Damage with jerk:                 $(round(100*(damage(lg; jerk=true)), digits=2)) %")
+    println("Combined performance γ: $(round(-100*result.bbo_best_feas[1], digits=2)) %")  
+    wcs
 end
 
 function copy_settings()
     cp("data/wc_settings.yaml", "data/wc_settings_tuned.yaml"; force=true)
+    load_settings("system_tuned.yaml")
 end
 function change_value(lines, varname, value::Union{Integer, Float64})
-    KiteUtils.change_value(lines, varname, repr(round(value, digits=4)))
+    KiteUtils.change_value(lines, varname, repr(round(value, digits=5)))
 end
 function update_settings(wcs::WCSettings)
     lines = KiteUtils.readfile("data/wc_settings_tuned.yaml") 
@@ -192,31 +152,20 @@ function update_settings(wcs::WCSettings)
     lines = change_value(lines, "pf_low:", wcs.pf_low)
     lines = change_value(lines, "if_low:", wcs.if_low)
     lines = change_value(lines, "pf_high:", wcs.pf_high)
-    lines = change_value(lines, "if_high:", wcs.if_high)    
+    lines = change_value(lines, "if_high:", wcs.if_high) 
+    lines = change_value(lines, "df_high:", wcs.df_high)    
     KiteUtils.writefile(lines, "data/wc_settings_tuned.yaml")
 end
 
-function tune_all()
-    wcs = autotune(wcsSpeedControl)
+function tune_all(max_iter=1000)
+    wcs = autotune(max_iter)
     if ! isnothing(wcs)
-        copy_settings()
         update_settings(wcs)
         println()
-        wcs = autotune(wcsLowerForceLimit)
-        if ! isnothing(wcs)
-            update_settings(wcs)
-            println()
-            wcs = autotune(wcsUpperForceLimit)    
-            if ! isnothing(wcs)
-                update_settings(wcs)
-            end
-        end
         @info "Tuned settings saved to data/wc_settings_tuned.yaml"
     end
 end
 
+copy_settings()
+tune_all(20)
 tune_all()
-tune_all()
-
-
-# autotune(wcsUpperForceControl)
