234647e5cc50722b56ad3784dcc02d32aae106de
Uwe Fechner <u.fechner-1@tudelft.nl>
2025-06-01 22:49:11 +0200

    mwe_01 works fine

--- mwes/mwe_01.jl
+++ mwes/mwe_01.jl
@@ -20,26 +20,31 @@ wcs.test = true
 winch = Winch(wcs, set)
 
 # find equilibrium speed
-function find_equilibrium_speed(winch, set_speed, force, n=100)
+function find_equilibrium_speed(winch, set_speed, force, n=2000)
     TIME = Float64[]
     V_SET = Float64[]
     V_ACT = Float64[]
     time = 0.0
+    last_v_act = 0.0
     # slow start
-    for v_set in range(0.0, set_speed, n)
+    for v_set in range(0.0, 2*set_speed, n)
+        lim_speed = minimum([v_set, set_speed])
         set_force(winch, force)
-        set_v_set(winch, v_set)
+        set_v_set(winch, lim_speed)
         v_act = get_speed(winch)
-        push!(V_SET, v_set)  
+        push!(V_SET, lim_speed)  
         push!(V_ACT, v_act)      
         on_timer(winch)
         push!(TIME, time)
         time += wcs.dt
+        if v_set > 0 && abs(v_act - last_v_act) < 1e-6
+            return v_act
+        end
+        last_v_act = v_act
     end
     set_v_set(winch, set_speed)
     on_timer(winch)
-    return TIME, V_SET, V_ACT
 end
 
-TIME, V_SET, V_ACT = find_equilibrium_speed(winch, 5.0, 1000.0)
-plot(TIME, [V_SET, V_ACT])
+v_act = find_equilibrium_speed(winch, 8.0, 1000.0)
+println("Equilibrium speed: ", v_act)
