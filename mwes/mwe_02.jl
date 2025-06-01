9a1c307eb6fd3097569f6b24af59d8dfd7b143d0
Uwe Fechner <u.fechner-1@tudelft.nl>
2025-06-01 23:00:55 +0200

    Implemente motor_dynamics()

--- mwes/mwe_02.jl
+++ mwes/mwe_02.jl
@@ -47,6 +47,15 @@ function find_equilibrium_speed(winch, set_speed, force, n=2000)
     on_timer(winch)
 end
 
+function motor_dynamics(x, u)
+    # x: state vector, e.g., [v_act]
+    # u: input vector, e.g., [v_set, force]
+    v_act = x[1]
+    v_set, force = u[1], u[2]
+    acc = calc_acceleration(winch.wm, v_act, force; set_speed = v_set)
+    return [acc]
+end
+
 v_set = 8.0
 force = 1000.0
 v_act = find_equilibrium_speed(winch, v_set, force)
