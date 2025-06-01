439557d80461e04de5ccb6e745fe6dc553343c24
Uwe Fechner <u.fechner-1@tudelft.nl>
2025-06-01 23:30:54 +0200

    Added bode plot

--- mwes/mwe_02.jl
+++ mwes/mwe_02.jl
@@ -57,7 +57,7 @@ function motor_dynamics(x, u)
 end
 
 v_set = 8.0
-force = 1000.0
+force = 2000.0
 v_act = find_equilibrium_speed(winch, v_set, force)
 
 x0 = [v_act]         # State at operating point
@@ -84,5 +84,14 @@ using ControlSystemsBase
 
 sys = ss(A, B, C, D)
 
+# 1. Extract B matrices
+B1 = B[:, 1]   # First input column (active)
+B2 = B[:, 2]   # Second input column (constant)
 
+# 2. Compute steady-state offset from constant input
+x_ss = -A \ (B2 * force)  # Equivalent to -inv(A) * B2 * force
 
+# 3. Create new system with only the first input
+sys_new = ss(A, B1, C, D[:, 1])
+
+bode_plot(sys_new; title="Bode Plot of Linearized Winch System")
