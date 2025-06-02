12d2663f1da1a22923104b7ef81c9c9a1dab0992
1-Bart-1 <bart@vandelint.net>
2025-06-02 13:00:35 +0200

    Lin model not very good

--- examples/test_mpc_controller.jl
+++ examples/test_mpc_controller.jl
@@ -26,20 +26,17 @@ N = length(lg)
 
 # 1. Include Wind Speed in the Model
 function f!(dx, x, u, d, _)
-    τ = u[1]
     v = x[1]
-    # v_wind = x[2] # if you want to estimate wind speed
+    τ = u[1]
     v_wind = d[1] # disturbance
     F = calc_force(v_wind, v)
-    α = calc_acceleration(winch, v, F; set_torque=τ)
-    dx[1] = α
-    # dx[2] = 0.0 # if you want to estimate wind speed
+    a = calc_acceleration(winch, v, F; set_torque=τ)
+    dx[1] = a
     nothing
 end
 function h!(y, x, d, _)
-    # v_wind = x[2] # if you want to estimate wind speed
-    v_wind = d[1] # disturbance
     v = x[1]
+    v_wind = d[1] # disturbance
     y[1] = v
     y[2] = calc_force(v_wind, v)
     nothing
@@ -53,17 +50,18 @@ model = setname!(NonLinModel(f!, h!, Ts, nu, nx, ny, nd); u=vu, x=vx, y=vy, d=vd
 plant = setname!(NonLinModel(f!, h!, Ts, nu, nx, ny, nd); u=vu, x=vx, y=vy, d=vd)
 
 linmodel = linearize(model, x=[0], u=[0], d=[0])
-α=0.01; σQ=[0.05]; σR=[0.5, 0.5]; nint_u=[1]; σQint_u=[0.1]; nint_ym=[0,0]
-kf = KalmanFilter(linmodel; σQ, σR, nint_u, σQint_u, nint_ym)
+α=0.01; σQ=[0.05]; σR=[0.5, 0.5]; nint_u=[0]; σQint_u=[0.1]; nint_ym=[0,0]
+kf = KalmanFilter(linmodel; σQ, σR, nint_u, nint_ym)
 
-Hp, Hc, Mwt, Nwt = 20, 2, [0.5, 0.0], [0.1]
+Hp, Hc, Mwt, Nwt = 20, 20, [0.5, 0.0], [0.1]
 umin, umax = [-100.0], [+100.0]
 ymin, ymax = [-Inf, wcs.f_low], [Inf, wcs.f_high]
-mpc = LinMPC(kf; Hp, Hc, Mwt, Nwt, Cwt=1e6)
+mpc = LinMPC(kf; Hp, Hc, Mwt, Nwt, Cwt=1e3)
 mpc = setconstraint!(mpc; umin, umax, ymin, ymax)
 
 function sim_adapt!(mpc, nonlinmodel, N, ry, plant, x_0, x̂_0, y_step=[0, 0])
-    U_data, Y_data, Ry_data = zeros(plant.nu, N), zeros(plant.ny, N), zeros(plant.ny, N)
+    U_data, Y_data, D_data, Ry_data, X̂_data, X_data = 
+        zeros(plant.nu, N), zeros(plant.ny, N), zeros(plant.nd, N), zeros(plant.ny, N), zeros(mpc.estim.nx̂, N), zeros(plant.nx, N)
     setstate!(plant, x_0)
     initstate!(mpc, [0], plant([0]), [0])
     setstate!(mpc, x̂_0)
@@ -76,16 +74,16 @@ function sim_adapt!(mpc, nonlinmodel, N, ry, plant, x_0, x̂_0, y_step=[0, 0])
         u = moveinput!(mpc, ry, d)
         linmodel = linearize(nonlinmodel; u, x=x̂[1], d) # disturbance
         setmodel!(mpc, linmodel)
-        U_data[:,i], Y_data[:,i], Ry_data[:,i] = u, y, ry
+        U_data[:,i], Y_data[:,i], D_data[:,i], Ry_data[:,i], X̂_data[:,i], X_data[:,i] = u, y, d, ry, x̂, plant.x0
         updatestate!(mpc, u, y, d) # update mpc state estimate
         updatestate!(plant, u, d)  # update plant simulator
     end
-    res = SimResult(mpc, U_data, Y_data; Ry_data)
+    res = SimResult(mpc, U_data, Y_data, D_data; Ry_data, X̂_data, X_data)
     return res
 end
 
 ry = [1.0, 0.0]
 x_0 = [0.0]
-x̂_0 = zeros(2)
+x̂_0 = zeros(1)
 @time res_slin = sim_adapt!(mpc, model, N, ry, plant, x_0, x̂_0)
-plot(res_slin)
+plot(res_slin; plotd=false, plotxwithx̂=true)
